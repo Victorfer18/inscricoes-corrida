289981d2dedbe89334c72a3aa43c90330e1e8f26
victor.magalhaes <victorfernandomagalhaes@gmail.com>
2025-09-07 13:42:29 -0300

    hotfix/theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen overflow-x-hidden">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-4 sm:px-6 flex-grow overflow-x-hidden">
